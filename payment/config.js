e46852a4889b30e8c3360747b896d95153b65440
konvkgiru <grigorydushek@gmail.com>
2026-04-12 16:10:09 +0300

    Create config.js

---
+++ payment/config.js
@@ -0,0 +1,14 @@
+// Конфигурация приложения
+const CONFIG = {
+    // API Crypto Pay (тестовая сеть)
+    API_URL: 'https://testnet-pay.crypt.bot/api',
+    
+    // Токен (НОВЫЙ! Не используйте старый)
+    API_TOKEN: 'ВАШ_НОВЫЙ_ТОКЕН',
+    
+    // Курс валют
+    RATE_USDT_TO_RUB: 77.43,
+    
+    // Данные платежа (берутся из URL)
+    invoiceData: null
+};
